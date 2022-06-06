62d319814f225de2f0c3cd222ea3e111bdb976bc
Ruby <v.grasso@itsrizzoli.com>
2022-06-06 10:16:36 +0200

    feat: feat

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -4,7 +4,7 @@ import {Button} from "qualcosa/atoms/button/Button"
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
-        <Button label={"somebody once told me the world is gonna roll me"} />
+        <Button label={"something"} />
     </div>
 );
 
